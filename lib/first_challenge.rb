544b11729148b854285428b1ade46906554577d4
rebeccaswebsite <rebecca.huseyin.14@ucl.ac.uk>
2019-03-20 17:30:43 +0000

    practice manipulating hashes:

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -14,9 +14,21 @@ def first_challenge
   }
 
   #your code here
-
+  contacts.each do |person, data|
+  #at this level, "person" is Jon Snow or Freddy and "data" is a hash of key/value pairs
+  #to iterate over the "data" hash, we can use the following line: 
+ 
+  data.each do |attribute, value|
+    #at this level, "attribute" describes the key of :name, :email, :favorite_icecream_flavors, or :knows
+    #we need to first check and see if the key is :favorite_icecream_flavors,
+    #if it is, that means the VALUE is an array that we can iterate over to print out each element
+ 
+    if attribute == :favorite_icecream_flavors
+        value.delete_if {|flavor| flavor ="strawberry" }
+        end
+      end
+    end
 
   #remember to return your newly altered contacts hash!
   contacts
 end
-
